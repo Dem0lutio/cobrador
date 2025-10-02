7f4e0f9324cbb642ec2a3e3f87ae886cb67ffe89
Luis Felipe de Azambuja Feyh <luisfelipe@feyh.com.br>
2025-10-02 10:43:55 -0300

    Autenticação com JWT completa, current user decorator e dto criados, JWT strategy e guard também, feature terminada.

--- backend/src/users/users.controller.ts
+++ backend/src/users/users.controller.ts
@@ -1,28 +1,28 @@
-import { UsersService } from "./users.service";
 import {
   Controller,
   Get,
   Post,
   Patch,
   Param,
-  Request,
   Body,
+  UseGuards,
 } from "@nestjs/common";
 import { CreateUserDto } from "./dtos/create-user.dto";
 import { AuthService } from "./auth/auth.service";
 import { SignInDto } from "./dtos/sign-in.dto";
+import { JwtAuthGuard } from "./auth/jwt-auth.guard";
+import { CurrentUser } from "./decorators/current-user.decorator";
+import { CurrentUserDto } from "./dtos/current-user.dto";
 
 @Controller("users")
 export class UsersController {
-  constructor(
-    private userservice: UsersService,
-    private authService: AuthService
-  ) {}
+  constructor(private authService: AuthService) {}
   // Controller methods will go here
 
   @Get("profile")
-  getProfile() {
-    console.log("getProfile called");
+  @UseGuards(JwtAuthGuard)
+  getProfile(@CurrentUser() user: CurrentUserDto) {
+    return { usuario_logado: user };
   }
 
   @Post("signup")
--- backend/src/users/users.module.ts
+++ backend/src/users/users.module.ts
@@ -5,18 +5,22 @@ import { AuthService } from "./auth/auth.service";
 import { PrismaService } from "src/infra/prisma/prisma.service";
 import { JwtModule } from "@nestjs/jwt";
 import { ConfigService } from "@nestjs/config";
+import { PassportModule } from "@nestjs/passport";
+import { JwtStrategy } from "./auth/jwt.strategy";
 
 @Module({
   imports: [
+    PassportModule,
     JwtModule.registerAsync({
       inject: [ConfigService],
       useFactory: (config: ConfigService) => ({
         secret: config.getOrThrow("JWT_SECRET"),
-        signOptions: { expiresIn: "60s" },
+        signOptions: { expiresIn: "7d" },
       }),
     }),
   ],
   controllers: [UsersController],
-  providers: [UsersService, AuthService, PrismaService],
+  providers: [UsersService, AuthService, PrismaService, JwtStrategy],
+  exports: [JwtStrategy],
 })
 export class UsersModule {}
